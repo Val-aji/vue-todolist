e0bcebb1d5435f04b4425d86cb8c92091379aab6
Nouval Aji Satrio <82368499+Val-aji@users.noreply.github.com>
2023-03-21 00:07:15 +0700

    Update LandingPage.vue

--- src/components/landingpage/LandingPage.vue
+++ src/components/landingpage/LandingPage.vue
@@ -56,7 +56,7 @@
                   const username = Cookies.get("username")
                   const formData = new FormData()
                   formData.append("username", username)
-                  const result = await instance().post("/getData" formData)
+                  const result = await instance().post("/getData", formData)
                   this.setValueData(result.data.data)
              },
             setIsLogin(res) {
@@ -77,7 +77,7 @@
     
         <div class="LandingPage">
             <AccountTodo @setIsLogin="setIsLogin" />
-            <NavbarHP @setData="setData" />
+            <NavbarHP @setData="setData" @setIsLogin="setIsLogin" />
             <ComponentCard :data="data" @deleteData="deleteData" @selesai="selesai"/>
         </div>
     
